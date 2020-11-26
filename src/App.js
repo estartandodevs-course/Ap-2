3ea836dd902c63bfec15dab9b881e0406a9fc543
MrVtR <30987772+MrVtR@users.noreply.github.com>
2020-11-26 00:19:58 -0300

    Adicionado função para mudar imagem após upload

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import './components/ImagemPerfil/ImagemPerfil'
 import ImagemPerfil from './components/ImagemPerfil/ImagemPerfil';
 
-function App() {
-  return (
-    <div>
-    <ImagemPerfil/>
-    </div>
-  );
-}
+function App() {return (<ImagemPerfil/>);}
 
 export default App;
