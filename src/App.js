aabb7a46b1a0d6f5538cf875b16e51f8725f938a
jessicarf18 <jessicarosalia678@gmail.com>
2020-11-29 21:55:04 -0300

    excluindo imports nao usados

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import React from "react";
 import { Routes } from "./pages/routes";
-import { RegisterFirstStep } from "./pages/RegisterFirstStep/RegisterFirstStep";
-import { RegisterSecondStep } from "./pages/RegisterSecondStep/RegisterSecondStep";
-import { RegisterThirdStep } from "./pages/RegisterThirdStep/RegisterThirdStep";
-import { RegisterFourthStep } from "./RegisterFourthStep/RegisterFourthStep";
 
 function App() {
   return (
