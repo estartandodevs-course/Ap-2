a2e8bac5b5540b37d3abe628897bc75f44687e52
PedrocaMarmota <pedrolucasdgm@gmail.com>
2020-12-01 16:06:17 -0300

    footer finalizado

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 import './footer.scss';
 
 
-export function footer() {
+export default function footer() {
     return (
-        <div class='footer'>
-            <Link to=''>Anterior</Link>
-            <Link to=''>Próximo</Link>
+        <div className='footer'>
+            <Link id='anterior' to=''>Anterior</Link>
+            <Link id='proximo' to=''>Próximo</Link>
         </div>
     )
 }
