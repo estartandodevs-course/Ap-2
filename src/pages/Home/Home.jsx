9f95885c2506165f6ae59bb7a01e8744ee280379
mayrazan <mayramanchein@yahoo.com.br>
2020-12-03 18:45:40 -0300

    criando botão e adicionando estilo

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React from "react";
+import { Button } from "../../components/Button/Button";
+import { useHistory } from "react-router-dom";
+import "./Home.scss";
 
-export function Home(){
-    return (
-        <div>TO DO HOME</div>
-    )
+export function Home() {
+  const history = useHistory();
+
+  function onClick() {
+    history.push("/");
+  }
+
+  return (
+    <div className="home-container">
+      <Button
+        width="136px"
+        background="#184177"
+        height="32px"
+        borderRadius="10px"
+        className="btn btn-profile btn-font1 btn-text"
+        onClick={onClick}
+      >
+        Editar Perfil
+      </Button>
+    </div>
+  );
 }
