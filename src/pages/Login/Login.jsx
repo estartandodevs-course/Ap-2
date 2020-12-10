dd3fb29d5dce5aa36652643c0a732bc07b4c5856
PedrocaMarmota <pedrolucasdgm@gmail.com>
2020-12-10 03:31:11 -0300

    campo de senha agora esconde a senha

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -21,6 +21,7 @@ export function Login() {
           <div className="input-container">
             <img src={email} alt="email"></img>
             <InputText
+            type='text'
               label="Email"
               isRequired={true}
               marginBottom="40px"
@@ -33,6 +34,7 @@ export function Login() {
           <div className="input-container">
             <img src={senha} alt="senha"></img>
             <InputText
+              type="password"
               label="Senha"
               isRequired={true}
               marginBottom="40px"
