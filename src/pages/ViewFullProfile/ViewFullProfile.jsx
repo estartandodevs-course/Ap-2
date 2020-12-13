083ed6a17e86b4276d2b42d8cc4b2075b5b07019
mayrazan <mayramanchein@yahoo.com.br>
2020-12-13 19:49:17 -0300

    Adicionando navBar

--- src/pages/ViewFullProfile/ViewFullProfile.jsx
+++ src/pages/ViewFullProfile/ViewFullProfile.jsx
@@ -6,31 +6,42 @@ import back from "../../assets/icons/arrow_back_ios_24px.svg";
 import { useState } from "react";
 import Typography from "../../components/Typography/Typography";
 import { useHistory } from "react-router-dom";
+import { NavBar } from "../../components/LayoutHome/NavBar/NavBar";
 
 export function ViewFullProfile() {
   const [currentProfile, setProfile] = useState(1);
   const history = useHistory();
-  
+
   let showCurrentProfile = userFullProfile.filter((option) => {
     return option.id === currentProfile;
   });
 
   return (
-    <div>
+    <div className="full-profile">
       {showCurrentProfile.map((option) => {
         return (
-          <RegisterHeader height="100%" width="auto" className="content">
-            <StepHeader
-              content="Voltar"
-              src={back}
-              alt="Voltar"
-              className="flex-start"
-              onClick={() => history.push("/profile")}
-            />
-            <Typography type="bigTitle">{option.name}</Typography>
-          </RegisterHeader>
+          <>
+            <RegisterHeader height="100%" width="auto" className="content">
+              <StepHeader
+                content="Voltar"
+                src={back}
+                alt="Voltar"
+                className="flex-start"
+                onClick={() => history.push("/profile")}
+              />
+              <Typography type="bigTitle" className="typography">
+                {option.name}
+              </Typography>
+            </RegisterHeader>
+
+            <div className="full-profile-content">
+
+
+            </div>
+          </>
         );
       })}
+      <NavBar statusSearch={true} statusHome={false} statusChat={false} />
     </div>
   );
 }
