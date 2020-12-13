bcee78a767d26ca0c9801008857d78fb68c20575
mayrazan <mayramanchein@yahoo.com.br>
2020-12-13 19:24:35 -0300

    Adicionando header

--- src/pages/ViewFullProfile/ViewFullProfile.jsx
+++ src/pages/ViewFullProfile/ViewFullProfile.jsx
@@ -1,5 +1,36 @@
 import "./ViewFullProfile.scss";
+import { userFullProfile } from "../../mocks/UserFullProfile";
+import { StepHeader } from "../../components/StepHeader/StepHeader";
+import { RegisterHeader } from "../../components/HeaderStep1/HeaderStep1";
+import back from "../../assets/icons/arrow_back_ios_24px.svg";
+import { useState } from "react";
+import Typography from "../../components/Typography/Typography";
+import { useHistory } from "react-router-dom";
 
 export function ViewFullProfile() {
-  return <div>Perfil Completo</div>;
+  const [currentProfile, setProfile] = useState(1);
+  const history = useHistory();
+  
+  let showCurrentProfile = userFullProfile.filter((option) => {
+    return option.id === currentProfile;
+  });
+
+  return (
+    <div>
+      {showCurrentProfile.map((option) => {
+        return (
+          <RegisterHeader height="100%" width="auto" className="content">
+            <StepHeader
+              content="Voltar"
+              src={back}
+              alt="Voltar"
+              className="flex-start"
+              onClick={() => history.push("/profile")}
+            />
+            <Typography type="bigTitle">{option.name}</Typography>
+          </RegisterHeader>
+        );
+      })}
+    </div>
+  );
 }
