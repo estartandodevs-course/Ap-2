5cca6ea34027303b1598ec1d873e6b2bfd68dc1d
mayrazan <mayramanchein@yahoo.com.br>
2020-12-17 22:03:30 -0300

    Adicionando função logout

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,16 +1,25 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 
-
 export async function login(email, password) {
-    console.log(email, password)
-    try {
-        const user = await firebase.auth().signInWithEmailAndPassword(email, password);
-        
-        return { data: user, success: true };
-    } catch (error) {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        return { data: { errorCode, errorMessage }, success: false };
-    }
+  console.log(email, password);
+  try {
+    const user = await firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password);
+
+    return { data: user, success: true };
+  } catch (error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    return { data: { errorCode, errorMessage }, success: false };
+  }
 }
+
+// export async function logout() {
+//   try {
+//     await firebase.auth().signOut();
+//   } catch(error){
+
+//   }
+// }
